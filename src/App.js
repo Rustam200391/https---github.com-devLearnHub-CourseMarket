a005cc9fd3e6107ee6053914f85834a7961c5100
Rustam Guseinov <rg20031991.com>
2023-04-27 15:04:48 +0300

    Change input style component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ import { Login } from "./pages/Login";
 export default function App() {
   return (
     <>
-
       <Router>
         <Routes>
           <Route path="/" element={<Form />} />
           <Route path="/login" element={<Login />} />
           {/* <Route path="/signup" component={Signup} /> */}
->>>>>>> 54e5f737fe58f2a8904eb9e876e9f1cf59ad8b94
         </Routes>
       </Router>
     </>
