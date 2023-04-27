f5d9d0bde40537007693909d40285817edf9c8fd
Sandra Zhitova <pomorkaa@yandex.ru>
2023-04-27 14:27:31 +0300

    Добавила базу данных, теперь работает, можно писать бэк

--- core/core/settings.py
+++ core/core/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(),
+    'default': env.db()
 }
 
 
