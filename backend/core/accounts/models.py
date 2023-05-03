3d3b4aad3d05c015564e5110fc9136e3efdc2102
Pomorka <al.zhitova@yandex.ru>
2023-05-03 11:43:28 +0300

    test

--- backend/core/accounts/models.py
+++ backend/core/accounts/models.py
@@ -4,12 +4,11 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField("Номер телефона", max_length=20, blank=True)
-    name = models.CharField('Имя', max_length=50)
-    surname = models.CharField('Фамилия', max_length=50)
-    email = models.EmailField('Почта')
-    password1 = models.CharField('Пароль', max_length=50)
-    password2 = models.CharField('Повторите пароль', max_length=50)
+    username = models.CharField('Имя', max_length=50)
+    mobile = models.CharField("Номер телефона", max_length=20, blank=True)
+    mail = models.EmailField('Почта')
+    password = models.CharField('Пароль', max_length=50)
+    confirmPassword = models.CharField('Повторите пароль', max_length=50)
 
     def str(self):
         return self.name
--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -38,7 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #"""приложения"""
+    'accounts',
+    # 'rest_framework',
+    
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
