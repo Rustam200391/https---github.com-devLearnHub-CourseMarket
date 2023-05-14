ac929b3220ba0ca4a2b6140abc4aa06dd9352f7b
Pomorka <al.zhitova@yandex.ru>
2023-05-14 15:01:50 +0300

    вход и регистрация на основе почты

--- backend/core/profiles/models.py
+++ backend/core/profiles/models.py
@@ -1,16 +1,57 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    """диспетчер моделей для User по почте"""
 
+    use_in_migrations = True
 
+    def _create_user(self, email, password, **extra_fields):
+        """Cоздать и сохранить пользователя по почте"""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Создать и сохранить обычного пользователя"""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Создать и сохранить суперюзера"""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+    
+    
 class UserData(AbstractUser):
+    """Модель пользователя"""
+    email = models.EmailField(_("email address"), unique=True)
     phoneNumber = PhoneNumberField(unique = True, null = False, blank = False, region="RU")
-    REQUIRED_FIELDS = ["phoneNumber"]
+    username = models.CharField(max_length=200, verbose_name="login")
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["phoneNumber","username"]
+
+    objects = UserManager()
 
 
 class Women(models.Model):
+    """Для проверки и отладки тестовая модель"""
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     create_time = models.DateTimeField(auto_now_add=True)
@@ -22,24 +63,3 @@ class Women(models.Model):
         return self.title
 
 
-
-
-# # Create your models here.
-# from django.db import models
-# from django.contrib.auth.models import User
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     username = models.CharField('Имя', max_length=50)
-#     mobile = models.CharField("Номер телефона", max_length=20, blank=True)
-#     mail = models.EmailField('Почта')
-#     password = models.CharField('Пароль', max_length=50)
-   
-
-#     def str(self):
-#         return self.username
-
-#     class Meta:
-#         verbose_name = 'Пользователь'
-#         verbose_name_plural = 'Пользователи'
