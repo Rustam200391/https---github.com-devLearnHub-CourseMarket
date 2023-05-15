a82052721d0c68a262e7f364cb0895c44d0c2584
AndrewArkhipov <AndrewArlhipow@yandex.ru>
2023-05-15 19:28:32 +0300

    валидация формы

--- frontend/src/pages/Registration/Form.jsx
+++ frontend/src/pages/Registration/Form.jsx
@@ -1,71 +1,104 @@
 import { Link } from "react-router-dom";
 import Button from '../../components/Button/Button';
 import style from './style.module.scss';
-import RegistrationItem from '../../components/Form/RegistrationItem.tsx';
+import RegistrationItem from '../../components/Form/RegistrationItem.jsx';
 import { useForm } from 'react-hook-form';
 
 
 export const Form = () => {
   
 
-  // const { register, handleSubmit, formState: { errors } } = useForm();
-  // const handleRegistration = (data) => console.log(data);
-  // const handleError = (errors) => {};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  // const registerOptions = {
-  //   name: { required: "Name is required" },
-  //   email: { required: "Email is required" },
-  //   password: {
-  //     required: "Password is required",
-  //     minLength: {
-  //       value: 8,
-  //       message: "Password must have at least 8 characters"
-  //     }
-  //   }
-  // };
-
+  const onSubmit = (data) => console.log(data);
+  const checkPassword = () => {
+    const password = document.getElementById("password").value;
+    const confirmpswd = document.getElementById("confirmpswd").value;
+    return password === confirmpswd || false;
+  };
 
   return (
-      <div className={style.form}>
-        <section className={style.form__container}>
+    <div className={style.form}>
+      <section className={style.form__container}>
         <h1 className={style.form__title}>
-        Sign
-          <span className={style.form__title, style.form__title_green }>
-          Up
-          </span>
+          Sign
+          <span className={style.form__title_green}>Up</span>
         </h1>
-        <form action="POST"    className={style.form__registration} >
-        {/* onSubmit={handleSubmit(handleRegistration, handleError)} */}
+        <form
+          action="POST"
+          className={style.form__registration}
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className={style.form__list}>
-            <RegistrationItem name="login"  />
-            {/* {...register('name', registerOptions.name) }
-            {errors?.name && errors.name.message} */}
+            <RegistrationItem title="login" error={errors.login}>
+              <input {...register("login", { required: true })} type="text" />
+            </RegistrationItem>
           </div>
+
           <div className={style.form__list}>
-            <RegistrationItem title="mobile number" type="tel" error={false}/>
+            <RegistrationItem title="Phone" error={errors.phone}>
+              <input
+                type="tel"
+                {...register("phone", { pattern: /^[0-9]+$/i, required: true })}
+              />
+            </RegistrationItem>
           </div>
+
           <div className={style.form__list}>
-            <RegistrationItem type="email" name="email" />
-            {/* {...register('email', registerOptions.email) } 
-            {errors?.email && errors.email.message} */}
+            <RegistrationItem title="email" error={errors.email}>
+              <input
+                type="text"
+                {...register("email", {
+                  pattern:
+                    /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
+                  required: true,
+                })}
+              />
+            </RegistrationItem>
           </div>
+
           <div className={style.form__list}>
-            <RegistrationItem title="password" type="password" error={false} />
-            {/* {...register('password', registerOptions.password) } 
-            {errors?.password && errors.password.message} */}
+            <RegistrationItem title="password" error={errors.password}>
+              <input
+                id="password"
+                type="password"
+                {...register("password", {
+                  required: true,
+                  validate: checkPassword,
+                })}
+              />
+            </RegistrationItem>
           </div>
+
           <div className={style.login__list}>
-            <RegistrationItem title="confirmPwd" type="password" error={false} />
+            <RegistrationItem
+              title="confirm password"
+              error={errors.confirmpwd}
+            >
+              <input
+                id="confirmpswd"
+                type="password"
+                {...register("confirmpwd", {
+                  required: true,
+                  validate: checkPassword,
+                })}
+              />
+            </RegistrationItem>
           </div>
-           
+
+          <div className={style.form__button}>
+            <Button text="Sign up" type="submit" />
+          </div>
         </form>
-        <div className={style.form__button}>
-        < Button text ="Sign up" type="submit" />
-        </div>
+
         <div className={style.links}>
           <Link to="/">Go home</Link>
         </div>
-        </section>
-      </div>
+      </section>
+    </div>
   );
 }
