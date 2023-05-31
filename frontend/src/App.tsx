c4da5c815480245d593591f9fb8b34073fc681a0
Rustam Guseinov <rg20031991.com>
2023-05-31 16:01:10 +0300

    correct path in app.tsx commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { Login } from "./pages/Login/Login.jsx";
 import { ResetPassword } from "./pages/ResetPwd/ResetPwd.jsx";
 import { ForgotPassword } from "./pages/ForgotPwd/ForgotPwd.jsx";
 import { DashBoard } from './pages/DashBoard/DashBoard.jsx';
-import  Order from "./Order.js";
 import './App.scss'
 import './assets/style/reset.css';
 
@@ -15,10 +14,10 @@ function App() {
     <>
       <Router>
         <Routes>
-
+          
+          {/* http://localhost:3000/:uid/:token/ переходить по этому пути */}
           <Route path="/:uid/:token/" element={<Login />} />
           {/*using the link from the mail to the login page, specify/{uid}/{token}/ */}
-           <Route path="order/:uid/:token" element={<Order />} />
 
           <Route path="/resetPassword/:uid:/:token" element={ResetPassword} />
           {/* // наверное такой путь  */}
